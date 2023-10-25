126946b4eb41e471a0c260b430f245d6417319ab
Ivan <ivan.ivkovic1996@gmail.com>
2023-10-25 21:15:28 +0200

    prvi radovi

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+// Navbar.js
+
+import React from 'react';
+// import './Navbar.css';
+
+const Contact = () => {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d699.3619722597488!2d16.41047145832905!3d45.48092475139186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4766f1df91098b13%3A0x53ccf72758f1642d!2sLi%C4%8Dka%20ul.%2021%2C%2044000%2C%20Sisak!5e0!3m2!1shr!2shr!4v1698251391400!5m2!1shr!2shr" width="600" height="450" style= {{ border: 0, allowfullscreen:"", loading: "lazy", referrerpolicy:"no-referrer-when-downgrade"}}></iframe>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+
+import React from 'react';
+// import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a href="#home">Početna</a>
+            <a href="#about">O nama</a>
+            <a href="#services">Usluge</a>
+            <a href="#contact">Kontakt</a>
+        </div>
+    );
+}
+
+export default Navbar;
